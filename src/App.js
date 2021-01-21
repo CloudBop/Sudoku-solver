097077e1ae2790c4ba5cf811aab412051ce6f813
Cloudbop <boppincolin@gmail.com>
2021-01-21 16:34:55 +0000

    added css, Banner/Tool implmnt handlers in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Banner } from './Components/Banner';
+import Tools from './Components/Tools'
+
+import React, { Component } from "react";
+//import './index.css';
+import "bulma/css/bulma.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  solve = () => {
+    console.log("Solve ");
+  };
+  stop = () => {
+    console.log("stop ");
+  };
+
+  newGame=()=>{ 
+    console.log("newGame" );
+  }
+  deleteGame=()=>{ 
+    console.log("deleteGame" );
+  }
+
+  getThisAsStr=()=>{ 
+    console.log("getThisAsStr" );
+  }
+  goBackT=()=>{ 
+    console.log("goBackT" );
+  }
+
+  componentDidMount() {}
+
+  render() {
+
+
+    return (
+      <div className="any">
+        <Banner />
+        <Tools 
+          solve={this.solve}
+          stop = { this.stop }
+          newGame = { this.newGame }
+          deleteGame = { this.deleteGame }
+          getThisAsStr = { this.getThisAsStr }
+          goBackT = { this.goBackT }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Banner = () => {
+
+  return (
+    <div className="hero">
+      <section className="hero-body">
+        <div className="container">
+          <h1 className="title">React </h1>
+          <h2 className="subtitle">
+            Sudoku with React
+          </h2>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+ 
+
+
+export  { Banner }  ;
