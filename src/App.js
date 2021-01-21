42913db88b91c2edf7bbfdabbb797582f14583eb
Cloudbop <boppincolin@gmail.com>
2021-01-21 17:41:07 +0000

    created Game class and added GameInfo stats

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@
 import './App.css';
 import { Banner } from './Components/Banner';
 import Tools from './Components/Tools'
-
 import React, { Component } from "react";
 //import './index.css';
 import "bulma/css/bulma.css";
 import Board from './Components/Board';
-
+import GameInfo from './Components/GameInfo';
+import NewGame from "./Controls/NewGame";
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.NewGame = new NewGame();
     this.state = {
-      cellValues: new Array(81).fill("5"),
-      cellsBackgroundColors: new Array(81).fill("bg-white")
+      cellValues: new Array(81).fill("2"),
+      cellsBackgroundColors: new Array(81).fill("bg-white") ,
+      gameLevel : null, 
+      complexityLevel : null, 
+      countEmptyCells : null,
+      complexityLog: 1,
     };
   }
+
+  resetColors = () => {
+    const colors = new Array(81).fill("bg-white");
+    this.setState({ cellsBackgroundColors: [...colors] });
+  };
+
+  makeCoral = () => {
+    const colors = new Array(81).fill("bg-coral");
+    this.setState({ cellsBackgroundColors: [...colors] });
+  };
+
+  loadAnewGame = () => {
+    let gameObj = this.NewGame.getFirstValue();
+    let level = gameObj.level ; 
+    console.log(" gameObj  ", gameObj ) ;
+    console.log("gameObj.str  ", gameObj.str);
+    let newArr = gameObj.str.split(";");
+    newArr.pop();
+    console.log("newArr ", newArr);
+    this.setState({ cellValues: [ ...newArr ] , gameLevel : level  });
+    this.resetColors();
+    // it will get a string value from Engine.js game String
+    //  var str = "5,,,1,,8,,,9,,,,6,,,,,2,,,2,,5,,,,6";
+    //const cells  = new Array(81).fill(  _.sample( [1,2,3,4,5,6,7,8,9] )    ) ;
+    //  this.setState( { cellValues : [...cells ] }) ;
+  };
 
   solve = () => {
     
@@ -37,6 +68,7 @@ class App extends Component {
 
   newGame=()=>{ 
     console.log("newGame" );
+    this.loadAnewGame();
   }
   deleteGame=()=>{ 
     console.log("deleteGame" );
@@ -57,29 +89,65 @@ class App extends Component {
     console.log("handleFocus e  : ", e);
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.loadAnewGame();
+  }
 
   render() {
 
 
     return (
-      <div className="any">
-        <Banner />
-        <Tools 
-          solve={this.solve}
-          stop = { this.stop }
-          newGame = { this.newGame }
-          deleteGame = { this.deleteGame }
-          getThisAsStr = { this.getThisAsStr }
-          goBackT = { this.goBackT }
-        />
+      <div>
+        <section className="hero is-fullheight">
+          <div className="container is-fluid">
+            <Banner />
 
-        <Board
-          handleChange={this.handleChange}
-          handleFocus={this.handleFocus}
-          cellValues={this.state.cellValues}
-          cellsBackgroundColors={this.state.cellsBackgroundColors}
-        />
+            <div className="container">
+              <Tools 
+                solve={this.solve}
+                stop = { this.stop }
+                newGame = { this.newGame }
+                deleteGame = { this.deleteGame }
+                getThisAsStr = { this.getThisAsStr }
+                goBackT = { this.goBackT }
+                />
+            </div>
+
+            <div className="container">
+
+              <div className="columns">
+
+                <div className="column">
+                  <Board
+                  handleChange={this.handleChange}
+                  handleFocus={this.handleFocus}
+                  cellValues={this.state.cellValues}
+                  cellsBackgroundColors={this.state.cellsBackgroundColors}
+                  />
+                </div>              
+                <div className="column">  
+                  <GameInfo
+                    gameLevel={this.state.gameLevel}
+                    complexityLevel={this.state.complexityLevel}
+                    countEmptyCells={this.state.countEmptyCells}
+                    complexityLog={this.state.complexityLog}
+                  /> 
+                </div>
+                <div className="column">
+                  <div className="columns">
+                    <div className="row">
+                      <div className="column"> Analysis of the game  </div>
+                      <div className="column">  Number of solved  </div>
+
+                      <div className="column">Input box</div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
